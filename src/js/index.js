80ad1729a7764b7985916afe6056b60c15623690
Jvgcgoncalves <goncalvesdacosta@gmail.com>
2023-03-31 16:59:59 -0300

    refactor:alter index.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
-var relogio= setInterval(function time(){
-    var h= document.getElementById('H')
-    var m= document.getElementById('M')
-    var s= document.getElementById('S')
-    var data= new Date()
-    var hora= data.getHours()
-    var minutos=data.getMinutes()
-    var segundos=data.getSeconds()
-    h.innerHTML=hora
-    m.innerHTML=minutos
-    s.innerHTML=segundos
+var relogio = setInterval(function time() {
+    var h = document.getElementById('H')
+    var m = document.getElementById('M')
+    var s = document.getElementById('S')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    var segundos = data.getSeconds()
+    h.innerHTML = hora
+    m.innerHTML = minutos
+    s.innerHTML = segundos
 }) 
